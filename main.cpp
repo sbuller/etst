3018c1abb8ef07668c5528cd5b58827dc541f777
Stefan Buller <stefan_buller@nova3.ca>
2015-11-03 14:15:32 -0600

    debuggable

--- main.cpp
+++ main.cpp
@@ -3,65 +3,160 @@
 #include <sstream>
 #include <vector>
 
-using namespace std;
+using namespace std;	//add
+
+struct Line;
+typedef string::size_type Width;	//some
 
 int tab_width = 4;
 
-int tsn(int xpos, int tsi) {	//lets try some
-	int next_ts = ((xpos / tab_width) + 1) * tab_width;	//comments
-	int spaces = (next_ts - xpos);	//inline
-	return spaces;
-}
+struct Line {		//comments
+	Line (string line) {	//to the file
+		auto pos = line.begin();
+		indent = 0;
 
-struct LineType {
-	int indent_level;
-	vector<string> cell;
-};
+		for (; pos < line.end() && *pos == '\t'; ++pos) indent++;
 
-int next_stop(int pos) {
-	return (pos / tab_width + 1) * tab_width;
-}
+		auto last = pos;
+		for (; pos < line.end(); ++pos) {
+			if (*pos == '\t') {
+				cell_widths.push_back(pos - last);
+				cells.push_back(string(last, pos));
+				last = pos;
+			}
+		}
+		cells.push_back(string(last, pos));
+	}
+	int stop_count() { return indent + cell_widths.size(); }
+	void print(vector<Width> stops) {
+		Width leading_indent_size = tab_width * indent;
+		auto cell = cells.begin();
+		auto last_cell = cells.end();
+		auto width_i = cell_widths.begin();
+		auto last_width_i = cell_widths.end();
+		Width w = leading_indent_size;
 
-string join(LineType line) {
-	stringstream ss;
-	int cur_width;
 
-	int indent_width = tab_width * line.indent_level;
-	ss <<string(indent_width, ' ');
-	cur_width = indent_width;
+		if (leading_indent_size) cout <<string(leading_indent_size, ' ');
+		if (!cells.empty()) {
+			cout <<"~"; cout.flush();
+			cout <<*cell;
+			w += *width_i;
+			cell++;
+			width_i++;
+		}
+		cout <<"`"; cout.flush();
 
-	for (auto s : line.cell) {
-		int w = cur_width + s.size();
+		if (!stops.empty()) {
+			auto stop = stops.begin() + indent;
+			while (cell < cells.end() && stop < stops.end()) {
+				Width d = *stop - w;
+				cout <<string(d, ' ');
+				w = *stop;
+				cout <<*cell;
+				w += *(width_i++);
+				cell++;
+				stop++;
+			}
+		}
+		cout <<"$" <<endl;
 	}
 
-	return ss.str();
-}
+	int indent;
+	vector<Width> cell_widths;
+	vector<string> cells;
+};
+
+struct Group {
+	Group() : stop_count(-1) { }
+
+	void reset() {
+		lines.clear();
+		cell_widths.clear();
+		stop_count = -1;
+	}
+
+	void add_line(Line l) {
+		if (l.stop_count() == stop_count) {
+			lines.push_back(l);
+			update_group_cell_widths(l);
+		} else {
+			lines.clear();
+			set_group_cell_widths(l);
+		}
+	}
+
+	void set_group_cell_widths(Line l) {
+		cell_widths.clear();
+		for (int i=0; i<l.indent; ++i) {
+			cell_widths.push_back(0);
+		}
+		for (auto cw : l.cell_widths) {
+			cell_widths.push_back(cw);
+		}
+	}
+
+	void update_group_cell_widths(Line l) {
+		int pos = l.indent;
+		for (auto cw : l.cell_widths) {
+			if (cw > cell_widths[pos]) cell_widths[pos] = cw;
+			pos++;
+		}
+	};
+
+	vector<Width> tabstops() {
+		int xpos = 0;
+		bool indent = true;
+		vector<Width> ts;
+		for (auto cell = cell_widths.begin(); cell < cell_widths.end(); cell++) {
+			if (*cell == 0 && indent) {
+				xpos += tab_width;
+			} else if (*cell == 0) {
+				xpos++;
+			} else {
+				xpos += *cell;
+				indent = false;
+			}
+			ts.push_back(xpos);
+		}
+		return ts;
+	}
+
+	void print() {
+		vector<Width> stops = tabstops();
+		for (auto line : lines) {
+			line.print(stops);
+		}
+	}
+
+	vector<Width> cell_widths;
+	vector<Line> lines;
+	int stop_count;
+};
 
 int main(int argc, char *argv[]) {
 	if (argc == 2) tab_width = atoi(argv[1]);
 
+	Group g;
+
 	for (string line; getline(cin, line); ) {
-		string::size_type pos;
-		bool in_indent = true;
-		int tsi = 0;
-		int xpos = 0;
+		Line l(line);
 
-		stringstream ss;
-
-		for (auto ch = line.begin(); ch != line.end(); ch++) {
-			if (*ch == '\t') {
-				int len = tsn(xpos, tsi);
-				ss <<string(len, ' ');
-				if (!in_indent) tsi++;
-				xpos += len;
-			} else {
-				in_indent = false;
-				xpos++;
-				ss <<*ch;
-			}
+		if (g.stop_count == l.stop_count()) {
+			cout <<"1: "; cout.flush();
+			g.add_line(l);
+		} else if (l.cells.size() <= 1) {
+			cout <<"2: "; cout.flush();
+			g.print();
+			g.reset();
+			l.print(vector<Width>());
+		} else {
+			cout <<"3: "; cout.flush();
+			g.print();
+			g.reset();
+			g.add_line(l);
 		}
-
-		cout <<ss.str() <<endl;
 	}
+	g.print();
 	return 0;
 }
